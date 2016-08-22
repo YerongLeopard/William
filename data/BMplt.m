47d8f35898a9800f7c02e5b9b88f0441c127ffb8
YerongLeopard <yerong.li@outlook.com>
2016-08-22 01:13:56 -0700

    hor-vert lines

--- data/BMplt.m
+++ data/BMplt.m
@@ -9,7 +9,7 @@ BMfitF = @(FBV, V)( ...
     FBV(1)+ ...
     9./16.* FBV(2)*FBV(4)* (... 
             ((FBV(4)./V).^(2./3.)-1).^3. * FBV(3) + ...
-            ((FBV(4)./V).^(2./3.)-1).^2..*(6- 4*(FBV(4)./V).^(2./3.)) ...
+            ((FBV(4)./V).^(2./3.)-1).^2. .*(6- 4*(FBV(4)./V).^(2./3.)) ...
                            ) ...
     );
 
@@ -17,9 +17,14 @@ figure; hold on;
 plot(LatticeC, F300, '*');
 xx = 4:0.05:8.;
 xxvol = xx*A*A*sqrt(3)/8;
-beta0 = [-9.1 0. 0. 8.86 ];
-opts = statset('MaxIter',9000);
+beta0 = [-9.3062    0.2469   10.9828    9.0445 ];
+% opts = statset('MaxIter',9000, 'TolFun', 1e-30);
+opts = statset('TolFun', 1e-100);
 beta = nlinfit(Volume, F300, BMfitF,beta0, opts);
-disp(sprintf('Optimal Lattice Constant C : %.4f', beta(4)/A/A/sqrt(3)*8.));
+optC = beta(4)/A/A/sqrt(3)*8.;
+disp(sprintf('Optimal Lattice Constant C : %.4f', optC));
 % BMfitF(beta0, Volume);
-plot(LatticeC, BMfitF(beta, Volume),'-');
+plot(xlim, [beta(1) beta(1)]);
+plot([optC optC], ylim);
+ylim
+plot(xx, BMfitF(beta, xxvol),'-');
