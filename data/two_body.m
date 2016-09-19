ded626569d5c320a62e5f8ff5c97e96897a43fa9
YerongLeopard <yerong.li@outlook.com>
2016-09-19 00:30:27 -0700

    plot for optimized LJ and X6

--- data/two_body.m
+++ data/two_body.m
@@ -1,16 +1,23 @@
 DATA = xlsread('data.xlsx','CCmd');
 %dim = size(DATA);
 LatticeC = DATA(:, 1);
-QM = DATA(:, 2); 
+QM = DATA(:, 2);
+xx = 3: 0.01: 3.6%intrapolation range
 X6S_original = DATA(:, 3); X6S_optimized = DATA(:, 4);
 LJ_original =DATA(:, 5); LJ_optimized = DATA(:, 6);
 X6_original = DATA(:, 7); X6_optimized = DATA(:, 8);
 CC= LatticeC / 2;
 figure; hold on;
-plotQM = plot (CC, QM, 'ok');
-plotX6S_original = plot(CC , X6S_original, 'xb'), plotX6S_optimized = plot(CC , X6S_optimized, '*b');
-plotLJ_original = plot(CC , LJ_original, 'xg'), plotLJ_optimized = plot(CC , LJ_optimized, '*g');
-plotX6_original = plot(CC , X6_original, 'xr'), plotX6_optimized = plot(CC , X6_optimized, '*r');
+plotQM = plot (CC, QM, 'ok','MarkerSize',10);
+plotX6S_original = plot(CC , X6S_original, 'xb','MarkerSize',10), plotX6S_optimized = plot(CC , X6S_optimized, '*b','MarkerSize',10);
+%yy = spline(CC,X6S_original,xx); plot(xx, yy, '-b');
+%yy = spline(CC,X6S_optimized,xx); plot(xx, yy, '-b');
+plotLJ_original = plot(CC , LJ_original, 'xg','MarkerSize',10), plotLJ_optimized = plot(CC , LJ_optimized, '*g','MarkerSize',10);
+yy = spline(CC,LJ_original,xx); plot(xx, yy, '-g');
+yy = spline(CC,LJ_optimized,xx); plot(xx, yy, '-g');
+plotX6_original = plot(CC , X6_original, 'xr','MarkerSize',10), plotX6_optimized = plot(CC , X6_optimized, '*r','MarkerSize',10);
+yy = spline(CC,X6_original,xx); plot(xx, yy, '-r');
+yy = spline(CC,X6_optimized,xx); plot(xx, yy, '-r');
 h = legend([plotQM, plotX6S_original, plotX6S_optimized, plotX6_original, plotX6_optimized, plotLJ_original, plotLJ_optimized], ...
     'QM', ...
     'X6S\_original', 'X6S\_optimized', ...
