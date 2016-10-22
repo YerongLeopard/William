5101e53a3d2c62e45006209440fb47713cea39fc
YerongLeopard <yerong.li@outlook.com>
2016-10-21 20:27:21 -0700

    plot precision files

--- data/precision.m
+++ data/precision.m
@@ -12,36 +12,50 @@ highQME   = DATA(:,8);
 highF0    = DATA(:,10);
 
 figure; hold on;
-xx = 6: 0.01: 7.2; %intrapolation range
+xx = 6.1: 0.01: 7.2; %intrapolation range
+x_search = LatticeC(8):0.00001:LatticeC(5);
 
 plot_lowQME    = plot(LatticeC,   lowQME, 'rs');
 yy = spline(LatticeC, lowQME, xx); plot(xx, yy, '-r');
+y_search =  spline(LatticeC, lowQME, x_search);
+x_min_low1 = x_search(find(y_search == min(y_search)))
 plot_lowF0     = plot(LatticeC,    lowF0, 'ro');
-yy = spline(LatticeC, lowF0, xx); plot(xx, yy, '-r');
+yy = spline(LatticeC,  lowF0, xx); plot(xx, yy, '-r');
+y_search =  spline(LatticeC, lowF0, x_search);
+x_min_low2 = x_search(find(y_search == min(y_search)))
 
 plot_mediumQME = plot(LatticeC,mediumQME, 'bs');
 yy = spline(LatticeC, mediumQME, xx); plot(xx, yy, '-b');
+y_search =  spline(LatticeC, mediumQME, x_search);
+x_min_med1 = x_search(find(y_search == min(y_search)))
 plot_mediumF0  = plot(LatticeC, mediumF0, 'bo');
-yy = spline(LatticeC, mediumF0, xx); plot(xx, yy, '-b');
+yy = spline(LatticeC,  mediumF0, xx);  plot(xx, yy, '-b');
+y_search =  spline(LatticeC, mediumF0, x_search);
+x_min_med2 = x_search(find(y_search == min(y_search)))
 
 plot_highQME   = plot(LatticeC,  highQME, 'ks');
 yy = spline(LatticeC, highQME, xx); plot(xx, yy, '-k');
+y_search =  spline(LatticeC, highQME, x_search);
+x_min_hig1 = x_search(find(y_search == min(y_search)))
 plot_highF0    = plot(LatticeC,   highF0, 'ko');
-yy = spline(LatticeC, highQME, xx); plot(xx, yy, '-k');
+yy = spline(LatticeC,  highF0, xx); plot(xx, yy, '-k');
+y_search =  spline(LatticeC, highF0, x_search);
+x_min_hig2 = x_search(find(y_search == min(y_search)))
 
- h = legend([plot_lowQME, plot_lowF0, ...
+
+h = legend([plot_lowQME, plot_lowF0, ...
              plot_mediumQME, plot_mediumF0, ...
              plot_highQME, plot_highF0], ...
-     ['F(0K): $c_{opt}$(nst) = ' num2str(optC_nst_f0) '$\AA$'], ...
-     ['F(0K): $c_{opt}$(full) = ' num2str(optC_full_f0) '$\AA$'], ...
-     ['QME: $c_{opt}$(nst) = ' num2str(optC_nst_qme) '$\AA$'], ...
-     ['QME: $c_{opt}$(full) = ' num2str(optC_full_qme) '$\AA$'], ...
-     ['QME: $c_{opt}$(full) = ' num2str(optC_full_qme) '$\AA$'], ...
-     ['QME: $c_{opt}$(full) = ' num2str(optC_full_qme) '$\AA$'] ...
+             ['QME: $c_{opt}$ = ' num2str(x_min_low1) '$\AA$;' 'SCF cutoff $10^{-6}$ - Plane wave cutoff 600'], ...
+             ['F(0K): $c_{opt}$ = ' num2str(x_min_low2) '$\AA$;' 'SCF cutoff $10^{-6}$ - Plane wave cutoff 600'], ...
+             ['QME: $c_{opt}$ = ' num2str(x_min_med1) '$\AA$;' 'SCF cutoff $10^{-8}$ - Plane wave cutoff 600'], ...
+             ['F(0K): $c_{opt}$ = ' num2str(x_min_med2) '$\AA$;' 'SCF cutoff $10^{-8}$ - Plane wave cutoff 600' ' False phonon'], ...
+             ['QME: $c_{opt}$ = ' num2str(x_min_hig1) '$\AA$;' 'SCF cutoff $10^{-8}$ - Plane wave cutoff 800'], ...
+             ['F(0K): $c_{opt}$ = ' num2str(x_min_hig2) '$\AA$;' 'SCF cutoff $10^{-8}$ - Plane wave cutoff 800'] ...
  );
 
 set(h, 'interpreter', 'latex');
-set(h, 'fontsize', 15);
+set(h, 'fontsize', 12);
 h = xlabel('Lattice constant c/ $\AA$');
 set(h,'interpreter','latex');
 h = ylabel('Energy/ $eV$ per atom');
