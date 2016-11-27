00de7a73cfb11708ccfad05354fe4bdfa0971df2
YerongLeopard <yerong.li@outlook.com>
2016-11-27 13:27:32 -0800

    force field optimization original and shifted

--- MD-X6b/graphite/con.sh
+++
@@ -1,6 +0,0 @@
-gnuplot set.gnp
-convert -density 500x500 -quality 100 h2.eps h2.jpg
-convert -density 500x500 -quality 100 h2o.eps h2o.jpg
-convert -density 500x500 -quality 100 hooh.eps hooh.jpg
-convert -density 500x500 -quality 100 h4o2.eps h4o2.jpg
-
--- MD-X6b/graphite/con2.awk
+++
@@ -1,3 +0,0 @@
-{
-if (substr($1,1,4)=="HOOH") print substr($1,6,9)
-}
